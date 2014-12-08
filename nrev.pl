870b1c1663ca39312fb959652c23e00070f82d5b
felix <felix@call-with-current-continuation.org>
2014-12-08 23:58:31 +0100

    Among other things: disable stupid git behaviour for adding newlines.

---
+++ nrev.pl
@@ -0,0 +1,16 @@
+
+append([],X,X).
+append([X|Y],Z,[X|W]) :- append(Y,Z,W).  
+
+% nrev/2 - naive reverse.
+nrev([ ],[ ]).
+nrev([X|Xs],Zs) :-
+         nrev(Xs,Ys),
+         append(Ys,[X],Zs).
+
+% nrev 30 has 496 logical inferences
+nrev30 :-
+	nrev([1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0], X),
+	write(X), nl.
+
+:- initialization(nrev30).
