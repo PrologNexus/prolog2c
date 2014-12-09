74ceab07506e0d3c07b2afbe494986fdf9e18133
felix <felix@call-with-current-continuation.org>
2014-12-10 00:24:48 +0100

    Added disjunction test from ISO book.

---
+++ tests/0016-disjunction-iso.pl
@@ -0,0 +1,14 @@
+% ISO-example to ";" operator
+
+:- include('prelude.pl').
+
+main :-
+	((insect(X), fly(X)); (has_legs(X, 6), fly(X))), display(X), nl, fail.
+main.
+
+insect(bee).
+insect(ant).
+
+fly(bee).
+
+has_legs(X, 6) :- insect(X).
