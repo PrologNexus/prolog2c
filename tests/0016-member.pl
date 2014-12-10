c253ba29355664c885d8999b95b4b163faab7b1b
felix <felix@call-with-current-continuation.org>
2014-12-10 13:51:27 +0100

    Added test for member/2.

---
+++ tests/0016-member.pl
@@ -0,0 +1,13 @@
+nl :- foreign_call(write_char(10)).
+
+display(X) :- foreign_call(basic_write(X)).
+
+main :-
+	member(X, [1,2,3]), display(X), nl, fail.
+main :-
+	member(2, [1,2,3]), display('yes'), nl,
+	(member(3, [1,2]); display('no'), nl),
+	member(1, Y), display(Y), nl.
+
+member(X, [X|_]).
+member(X, [_|R]) :- member(X, R).
