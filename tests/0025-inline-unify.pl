d9c138e188aa53d678c5456522a0b8fddeb69dd3
felix <felix@call-with-current-continuation.org>
2014-12-14 20:07:50 +0100

    Test for inline-unification.

---
+++ tests/0025-inline-unify.pl
@@ -0,0 +1,11 @@
+main :- 123 = 123, X = X, Y = 123,
+	Z = Y, display('1\n'),
+	X = Y, display('2\n'),
+	X = 123, display('3\n'),
+	display([X, Y, Z]), nl,
+	f(X) = f(123).
+
+nl :- foreign_call(write_char(10)).
+
+display(X) :- foreign_call(basic_write(X)).
+
