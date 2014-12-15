408feeb2b57dc864293b5704f13e242a3e0b8060
felix <felix@call-with-current-continuation.org>
2014-12-15 23:48:27 +0100

    Added simple GC test.

---
+++ tests/0032-simple-gc.pl
@@ -0,0 +1,16 @@
+display(X) :- foreign_call(basic_write(X)).
+nl :- foreign_call(write_char(10)).
+
+main :-
+	iota(1000, X),
+	foreign_call(gc),
+	dump(X).
+
+iota(N, L) :- iota(0, N, L).
+iota(N, N, []) :- !.
+iota(N, M, [N|R]) :-
+	!, N2 is N + 1, iota(N2, M, R).
+
+dump([]) :- nl.
+dump([X|Y]) :-
+	!, display(X), display(' '), dump(Y).
