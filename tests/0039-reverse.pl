23410e058c1548c09ae2f7874fcd9a706d304c6d
felix <felix@call-with-current-continuation.org>
2014-12-19 17:29:11 +0100

    More tests. Note that 0038-tak fails due to trail-overflow.

---
+++ tests/0039-reverse.pl
@@ -0,0 +1,16 @@
+:- include('prelude.pl').
+
+iota(N, L) :- iota(0, N, L).
+iota(N, N, []).
+iota(N, M, [N|R]) :-
+	N2 is N + 1, iota(N2, M, R).
+
+main :-
+	iota(100, L),
+	display(L), nl,
+	reverse(L, X),
+	display(X), nl.
+
+reverse(L, R) :- reverse(L, R, []).
+reverse([],Z,Z).
+reverse([H|T],Z,Acc) :- reverse(T,Z,[H|Acc]).
---
+++ tests/0040-set-operations.pl
@@ -0,0 +1,32 @@
+nl :- foreign_call(write_char(10)).
+display(X) :- foreign_call(basic_write(X)).
+member(X, [X|_]).
+member(X, [_|R]) :- member(X, R).
+
+iota(N, L) :- iota(0, N, L).
+iota(N, N, []).
+iota(N, M, [N|R]) :-
+	N2 is N + 1, iota(N2, M, R).
+
+main :-
+	union([1,2,3,4,5],[3,4,5,6,7],X),
+	display(X), nl,
+	intersection([1,2,3,4,5],[3,4,5,6,7],Y),
+	display(Y), nl,
+	difference([1,2,3,4,5],[3,4,5,6,7],Z),
+	display(Z), nl.
+
+union([], X, X).
+union([X|R], Y, Z):- member(X, Y), !, union(R, Y, Z).
+union([X|R], Y, [X|Z]):- union(R, Y, Z).
+
+intersection([], X, []).
+intersection([X|R], Y, [X|Z]) :- member(X, Y), !, intersection(R, Y, Z).
+intersection([X|R], Y, Z) :- intersection(R, Y, Z).
+
+difference([], _, []) :- !.
+difference([A|C], B, D) :-
+        member(A, B), !,
+        difference(C, B, D).
+difference([A|B], C, [A|D]) :-
+        difference(B, C, D).
