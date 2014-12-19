e208ee67627be60cabe7689a6966dc839d8e86b5
felix <felix@call-with-current-continuation.org>
2014-12-19 17:29:26 +0100

    Added some list-operations to prelude.

--- prelude.pl
+++ prelude.pl
@@ -17,3 +17,19 @@ append([],X,X).
 append([X|Y],Z,[X|W]) :- append(Y,Z,W).
 
 command_line_arguments(X) :- foreign_call(command_line_arguments(X)).
+
+reverse(L, R) :- reverse(L, R, []).
+reverse([],Z,Z).
+reverse([H|T],Z,Acc) :- reverse(T,Z,[H|Acc]).
+
+union([], X, X).
+union([X|R], Y, Z):- member(X, Y), !, union(R, Y, Z).
+union([X|R], Y, [X|Z]):- union(R, Y, Z).
+
+intersection([], X, []).
+intersection([X|R], Y, [X|Z]) :- member(X, Y), !, intersection(R, Y, Z).
+intersection([X|R], Y, Z) :- intersection(R, Y, Z).
+
+difference([], _, []) :- !.
+difference([A|C], B, D) :- member(A, B), !, difference(C, B, D).
+difference([A|B], C, [A|D]) :- difference(B, C, D).
