c2439a9e50e114efacb82c08d4778f96ba0b56ed
felix <felix@call-with-current-continuation.org>
2014-12-23 19:07:23 +0100

    see/1 and tell/1 handle stream argument.

--- io.pl
+++ io.pl
@@ -3,13 +3,16 @@
 
 see(user) :-
 	foreign_call(set_current_input_stream(0)).
+see(S) :-
+	stream(S),
+	!,
+	foreign_call(set_current_input_stream(S)).	
 see(NAME) :-
 	foreign_call(open_stream(NAME, 1, S)),
 	foreign_call(set_current_input_stream(S)).
 
 seeing(S) :- goreign_call(current_input_stream(S)).
 
-%XXX doesn't handle case when stdin is closed
 seen :-
 	foreign_call(current_input_stream(S)),
 	foreign_call(close_stream(S)),
@@ -17,13 +20,16 @@ seen :-
 
 tell(user) :-
 	foreign_call(set_current_output_stream(0)).
+tell(S) :-
+	stream(S), 
+	!,
+	foreign_call(set_current_output_stream(S)).
 tell(NAME) :-
 	foreign_call(open_stream(NAME, 0, S)),
 	foreign_call(set_current_output_stream(S)).
 
 telling(S) :- foreign_call(current_output_stream(S)).
 
-%XXX doesn't handle case when stdout is closed
 told :-
 	foreign_call(current_output_stream(S)),
 	foreign_call(close_stream(S)),
