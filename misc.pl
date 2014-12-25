23912bc909f07c054eea78bba27a5cd2457497fe
felix <felix@call-with-current-continuation.org>
2014-12-25 23:38:31 +0100

    Untested implementations of name/2 and univ

--- misc.pl
+++ misc.pl
@@ -16,3 +16,31 @@ throw(EXN) :-
 
 shell(CMD) :- shell(CMD, 0).
 
+name(X, S) :-
+	var(X), !,
+	(number_codes(X, S); atom_codes(X, S)).
+name(X, S) :-
+	number(X), ! number_codes(X, S).
+name(X, S) :- atom_codes(X, S).
+
+
+[X|Y] =.. Z :- !, Z = ['.', X, Y].
+X =.. Y :- atomic(X), !, Y = [X].
+X =.. [Y] :- !, atomic(Y), X = Y.
+X =.. Y :-
+	compound(X), !,
+	functor(X, NAME, ARITY),
+	Y = [NAME|ARGS],
+	'$univ_args'(X, 1, ARITY, ARGS).
+X =.. [N|ARGS] :-
+	var(X),
+	length(ARGS, ARITY),
+	functor(X, N, ARITY),
+	'$univ_args'(X, 1, ARITY, ARGS).
+
+'$univ_args'(TERM, I, I, []) :- !.
+'$univ_args'(TERM, I, N, [X|MORE]) :-
+	I =< N,
+	arg(I, TERM, X),
+	I2 is I + 1,
+	'$univ_args'(TERM, I2, N, MORE).
