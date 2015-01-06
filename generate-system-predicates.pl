7723f9e360f460c7c9a640579ea8ae64afb36fb0
felix <felix@call-with-current-continuation.org>
2015-01-06 15:27:06 +0100

    Added program to generate tables of system-predicates and invocation wrappers for interp.pl

---
+++ generate-system-predicates.pl
@@ -0,0 +1,44 @@
+%%% generate include-files for interp.pl from list of system-predicates
+
+
+main :-
+	op(200, fy, '#'),
+	op(200, fy, '?'),
+	tell('system_predicate.pl'),
+	telling(SP),
+	tell('call_primitive.pl'),
+	telling(CP),
+	run(SP, CP),
+	tell(SP), told,
+	tell(CP), told.
+
+run(SP, CP) :-
+	repeat,
+	read(TERM),
+	gen_sys_pred(TERM, SP),
+	gen_call_prim(TERM, CP),
+	TERM == end_of_file.
+
+gen_sys_pred(DEF, SP) :-
+	(DEF = system_predicate(PRED, _); DEF = system_predicate(PRED)),
+	!,
+	functor(PRED, NAME, ARITY),
+	tell(SP),
+	writeq(system_predicate(NAME, ARITY)), put(46), nl.
+gen_sys_pred(_, _).
+
+gen_call_prim(DEF, CP) :-
+	(DEF = system_predicate(PRED, _); DEF = system_predicate(PRED)),
+	!,
+	functor(PRED, NAME, ARITY),
+	build_lists(1, ARITY, TERM, ARGS, CALLARGS),
+	CALL =.. [NAME|CALLARGS],
+	tell(CP),		    
+	write((call_primitive(NAME, ARITY, TERM) :- !, ARGS, CALL)),
+	display('.\n').	
+gen_call_prim(_, _).
+
+build_lists(I, N, _, true, []) :- I > N, !.
+build_lists(I, N, T, (arg(I, T, X), VARS), [X|ARGS]) :-
+	I2 is I + 1,
+	build_lists(I2, N, T, VARS, ARGS).
