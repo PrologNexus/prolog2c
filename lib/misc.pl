9d4287b624e3f484e9d392231ad4046624c12f58
felix <felix@call-with-current-continuation.org>
2015-01-08 15:34:31 +0100

    Replaced throw/1 with call to primitive.

--- lib/misc.pl
+++ lib/misc.pl
@@ -5,14 +5,9 @@ compare(>, X, Y) :- X @> Y, !.
 compare(<, X, Y) :- X @< Y, !.
 compare(=, X, X).
 
-
-%% this is just a fake, to have some sort of error-signalling operation
-
-throw(EXN) :-
-	foreign_call(current_error_stream(S)),
-	foreign_call(set_current_output_stream(S)),
-	display('\nERROR: '), display(EXN), nl,
-	halt(70).
+throw(BALL) :-
+	copy_term(BALL, B2),
+	foreign_call(do_throw(B2)).
 
 shell(CMD) :- shell(CMD, 0).
 
