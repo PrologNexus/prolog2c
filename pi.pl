bab9dad29e2684e4678614aac80ea9971bbebedf
felix <felix@call-with-current-continuation.org>
2015-01-15 14:17:20 +0100

    Seperated core interpreter from command-line driver and repl, added printing of variables and optional backtracking.

---
+++ pi.pl
@@ -0,0 +1,62 @@
+%%%% interpreter toplevel
+
+
+:- include('interp.pl').
+
+main :-
+	global_set(trace_depth, none),
+	command_line_arguments(ARGS),
+	parse_arguments(ARGS),
+	!,
+	(recorded(initialization_goal, G) -> run_goal(G, []); repl).
+
+repl :-
+	display('?- '), flush,
+	read(TERM, VARS), 
+	(TERM == end_of_file, halt
+	; catch(run_goal(TERM, VARS), EXN, report_exception(EXN))),
+	!,			% force tailcall
+	repl.
+repl :-
+	display('\nno.\n'),
+	repl.
+
+run_goal(G, VARS) :-
+	execute(G),
+	show_variables(VARS),
+	display(' ? '), flush,
+	(get_response -> fail; display('\nyes.\n')).
+
+get_response :-
+	get0(59), skip_line.
+
+skip_line :-
+	repeat, get0(10), !.
+
+show_variables([]) :- !.
+show_variables([NAME=X|MORE]) :-
+	nl, display(NAME), display(' = '), writeq(X),
+	show_variables(MORE).
+
+report_exception(EXN) :-
+	display('\nUncaught exception:\n'),
+	writeq(EXN), nl.
+
+parse_arguments([]).
+parse_arguments(['-h'|_]) :-
+	usage(0).
+parse_arguments(['-t'|MORE]) :-
+	global_set(trace_depth, 0),
+	parse_arguments(MORE).
+parse_arguments(['-i', G|MORE]) :-
+	recordz(initialization_goal, G),
+	parse_arguments(MORE).
+parse_arguments([FILENAME|_]) :-
+	name(FILENAME, [45|_]), usage(1).
+parse_arguments([FILENAME|MORE]) :-
+	consult(FILENAME),
+	parse_arguments(MORE).
+
+usage(CODE) :-
+	display('usage: pi [-h] [-t] FILENAME ...\n'),
+	halt(CODE).
