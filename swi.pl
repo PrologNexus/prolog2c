69380b509d0b36dfe77d0ef6d23366072ccf5874
felix <felix@call-with-current-continuation.org>
2015-01-28 18:04:24 +0100

    Added SWI-specific variant of hash_atom/2

--- swi.pl
+++ swi.pl
@@ -12,3 +12,14 @@ dbreference(_) :- fail.		% sufficient here
 enable_trace(_).
 
 current_error_output(S) :- current_output(S).
+
+%% this must produce identical results as hash_name() in pc.h
+hash_atom(A, H) :-
+	atom_codes(A, AL),
+	hash_atom(0, AL, 0, H).
+hash_atom(I, AL, H, H) :-
+	(I > 100; AL == []), !.
+hash_atom(I, [C|R], H1, H2) :-
+	H is (H1 xor ((H1 << 6) + (H1 >> 2) + C)) /\ 2147483647,
+	I2 is I + 1,
+	hash_atom(I2, R, H, H2).
