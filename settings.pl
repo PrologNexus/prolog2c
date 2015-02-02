811355b7bccd1ee0cb17ab1f93d1c7a48181daf0
Felix Winkelmann <winkelmann@mps.mpg.de>
2015-02-02 12:37:08 +0100

    fixnum-range needs to be reduced for 32-bit platforms.

--- settings.pl
+++ settings.pl
@@ -4,4 +4,4 @@
 default_setting(output_file, user).
 default_setting(entry_point, '$start').
 default_setting(include_path, ['.']).
-default_setting(literal_fixnum_range, -2147483648 - 2147483647).
+default_setting(literal_fixnum_range, -1073741824 - 1073741823).
