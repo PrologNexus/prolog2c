a49302f4085cc7f09abba4364ce70a4b57fbd1cc
felix <felix@call-with-current-continuation.org>
2015-02-03 18:13:28 +0100

    Added sizing factor for atom-dispatch tables.

--- settings.pl
+++ settings.pl
@@ -5,4 +5,5 @@ default_setting(output_file, user).
 default_setting(entry_point, '$start').
 default_setting(include_path, ['.']).
 default_setting(atom_table_index_threshold, 10).
+default_setting(atom_table_size_factor, 3).
 default_setting(literal_fixnum_range, -1073741824 - 1073741823).
