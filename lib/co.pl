332d26f157b6a466977c5fbb439832565faa7778
felix <felix@call-with-current-continuation.org>
2015-02-13 15:05:18 +0100

    (disabled) debug output in co.pl

--- lib/co.pl
+++ lib/co.pl
@@ -2,14 +2,18 @@
 
 
 '$delay_goal'(VAR, PTR, ARGS) :-
-	var(VAR) -> foreign_call(delay_goal(VAR, PTR, ARGS))
-	; '$call_predicate'(PTR, ARGS).
+	%display([delay, VAR, PTR, ARGS]), nl,
+	(var(VAR) -> foreign_call(delay_goal(VAR, PTR, ARGS))
+	; '$call_predicate'(PTR, ARGS)
+	).
 
 '$freeze_goal'(VAR, PTR, ARGS) :-
-	var(VAR) ->
-	'$predicate_address'('$freeze_goal'/3, PTR2),
-	delay(VAR, '$call_predicate'(PTR2, [VAR, PTR, ARGS]))
-	; '$call_predicate'(PTR, ARGS).
+	%display([freeze, VAR, PTR, ARGS]), nl,
+	(var(VAR) ->
+	 '$predicate_address'('$freeze_goal'/3, PTR2),
+	 delay(VAR, '$call_predicate'(PTR2, [VAR, PTR, ARGS]))
+	; '$call_predicate'(PTR, ARGS)
+	).
 
 dif(X, Y) :-
 	foreign_call(special_id(X, Y, V)),
