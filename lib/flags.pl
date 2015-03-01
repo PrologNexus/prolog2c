f8202bb4c7bf86f6a60ddfa70bd1601a62da5e50
felix <felix@call-with-current-continuation.org>
2015-03-01 18:53:33 +0100

    Added lib/flags

---
+++ lib/flags.pl
@@ -0,0 +1,13 @@
+%%% prolog flags
+
+
+current_prolog_flag(version, 3).
+
+current_prolog_flag(prolog_title, '?-Prolog').
+current_prolog_flag(prolog_copyright, '(c)MMXV Felix L. Winkelmann').
+current_prolog_flag(bounded, true).
+current_prolog_flag(max_arity, 999). % arbitrary
+current_prolog_flag(integer_rounding_function, toward_zero). %XXX correct?
+current_prolog_flag(max_integer, N) :- foreign_call(fixnum_bounds(_, N)).
+current_prolog_flag(min_integer, N) :- foreign_call(fixnum_bounds(N, _)).
+current_prolog_flag(unknown, error).
