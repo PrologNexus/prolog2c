e251ef72dbfc91da753201c42418d4172bd70fae
Felix Winkelmann <winkelmann@mps.mpg.de>
2015-03-03 12:58:45 +0100

    Fixed test 0108

--- tests/0108-acyclic_term.pl
+++ tests/0108-acyclic_term.pl
@@ -2,7 +2,7 @@ main :-
 	f(R) = W, acyclic_term(W),
 	Q = f(Q), \+acyclic_term(Q),
 	X = f(Y), acyclic_term(X),
-	X = Q, \+acyclic_term(X), \+acyclic_term(X),
+	X = Q, \+acyclic_term(X), \+acyclic_term(Q),
 	foo(A, f(A)), \+acyclic_term(A).
 
 foo(X, X).
