0e96af044c22ffa5d54d548b84c192321f373c29
Felix Winkelmann <winkelmann@mps.mpg.de>
2015-03-03 14:34:31 +0100

    Added cycle-tests for ,copy_term/dup._term

--- tests/0109-simple-cycles.pl
+++ tests/0109-simple-cycles.pl
@@ -3,4 +3,6 @@ main :-
 	\+acyclic_term(A),
 	recordz(foo, A),
 	recorded(foo, X), \+acyclic_term(X),
-	A = X.
+	A = X,
+	copy_term(A, A2), \+acyclic_term(A2),
+	duplicate_term(A, A3), \+acyclic_term(A3).
