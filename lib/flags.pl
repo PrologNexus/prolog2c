1a3bce2939f40c9604e5d2355c756794b2897024
felix <felix@call-with-current-continuation.org>
2015-03-08 23:16:25 +0100

    changed arbitrary value arbitrarily

--- lib/flags.pl
+++ lib/flags.pl
@@ -6,7 +6,7 @@ current_prolog_flag(version, 3).
 current_prolog_flag(prolog_title, '?-Prolog').
 current_prolog_flag(prolog_copyright, '(c)MMXV Felix L. Winkelmann').
 current_prolog_flag(bounded, true).
-current_prolog_flag(max_arity, 999). % arbitrary
+current_prolog_flag(max_arity, 9999). % arbitrary
 current_prolog_flag(integer_rounding_function, toward_zero). %XXX correct?
 current_prolog_flag(max_integer, N) :- foreign_call(fixnum_bounds(_, N)).
 current_prolog_flag(min_integer, N) :- foreign_call(fixnum_bounds(N, _)).
