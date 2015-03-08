bc541162e9d5ebd2554ee46bb7c106c9f36eec75
felix <felix@call-with-current-continuation.org>
2015-03-08 16:26:11 +0100

    increased table-thesholds

--- settings.pl
+++ settings.pl
@@ -4,8 +4,8 @@
 default_setting(output_file, user).
 default_setting(entry_point, '$start').
 default_setting(include_path, ['.']).
-default_setting(integer_table_index_threshold, 1). %XXX  % only used in fact-blocks
-default_setting(atom_table_index_threshold, 1). %XXX
+default_setting(integer_table_index_threshold, 5).  % only used in fact-blocks
+default_setting(atom_table_index_threshold, 10).
 default_setting(structure_table_index_threshold, 5).
 default_setting(dispatch_table_size_factor, 3).
 default_setting(literal_fixnum_range, -1073741824 - 1073741823).
