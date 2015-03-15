4e9748e0bb4c3432b83072acbc1fde73d1fb2fa4
felix <felix@call-with-current-continuation.org>
2015-03-15 14:17:24 +0100

    atomic_list_concat is already available

--- tests/0071-atomic_list_concat.pl
+++ tests/0071-atomic_list_concat.pl
@@ -1,8 +1,3 @@
-atomic_list_concat(AL, A) :-
-	findall(AA, (member(A, AL), name(A, AA)), LL),
-	concatenate(LL, L),
-	atom_codes(A, L).
-
 main :-
 	atomic_list_concat([abc,'123.0',45,9.1,'___'], X), writeq(X), nl.
 
