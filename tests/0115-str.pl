d0025b4e96e703e49b29714c01f340cf65cf084f
felix <felix@call-with-current-continuation.org>
2015-03-15 14:17:36 +0100

    atomic_list_concat tested somewhere else

--- tests/0115-str.pl
+++ tests/0115-str.pl
@@ -1,7 +1,4 @@
 main :-
-	atomic_list_concat([this, 123, x], this123x),
-	atomic_list_concat([abc], abc),
-	atomic_list_concat([], ''),
 	split_string("//home/foo///bar/", "/", "", ["home", "foo", "bar"]),
 	split_string("//home/foo///   bar/", "/", "/ ", ["home", "foo", "bar"]),
 	split_string("   word ", "", " ", ["word"]).
