baa37e7564c357a03da46a77fd3250fc6a260dfe
felix <felix@call-with-current-continuation.org>
2015-03-16 23:51:16 +0100

    Updated self-compile benchmark

--- benchmarks/0006-self-compile.pl
+++ benchmarks/0006-self-compile.pl
@@ -8,17 +8,13 @@
 :- include('process.pl').
 :- include('compile.pl').
 :- include('assemble.pl').
-
-atomic_list_concat(AL, A) :-
-	findall(AA, (member(A, AL), name(A, AA)), LL),
-	concatenate(LL, L),
-	atom_codes(A, L).
+:- include('xref.pl').
 
 :- initialization(main).
 
 main :-
 	recordz(source_file, 'pc.pl'),
-	recordz(include_path, ['.']),
+	recordz(library_dir, ['.']),
 	recordz(output_file, '/dev/null'),
 	recordz(silent, yes),
 	compile_file('pc.pl').
