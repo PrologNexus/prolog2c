557db48c4341714860d0d06ed1782a51c9668997
felix <felix@call-with-current-continuation.org>
2015-03-20 20:46:59 +0100

    test for stream-props

---
+++ tests/0121-streamprops.pl
@@ -0,0 +1,10 @@
+main :-
+	display('current_input:\n'), props(current_input),
+	display('current_input:\n'), props(current_output),
+	open('tests/0121-streamprops.pl', read, S),
+	display('file:\n'), props(S),
+	close(S).
+
+props(S) :-
+	stream_property(S, P), writeq(P), nl, fail.
+props(_).
