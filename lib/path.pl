1e37afcafba355c1ac36868f712afb0ab644361e
felix <felix@call-with-current-continuation.org>
2015-03-24 00:51:57 +0100

    lib/path: added comments and strip_suffix/2

--- lib/path.pl
+++ lib/path.pl
@@ -1,6 +1,9 @@
 %%%% pathname operations
 
 
+%% return canonical path (with "..", extra slashes and "." removed),
+%% as absolute path. All predicates accept strings or atoms
+
 canonical_path(IN, OUT) :-
 	absolute_pathname(IN, ALST),
 	split_string(ALST, "/", "", OUTLST),
@@ -13,6 +16,8 @@ canonicalize_pathname(["."|R], R2) :- canonicalize_pathname(R, R2).
 canonicalize_pathname([X, ".."|R], R2) :- canonicalize_pathname(R, R2).
 canonicalize_pathname([X|R], [[47|X]|R2]) :- canonicalize_pathname(R, R2).
 
+%% make pathname absolute (relative to PWD)
+
 absolute_pathname(FNAME, ANAME) :-
 	atom(FNAME), atom_codes(FNAME, FLST),
 	absolute_pathname(FLST, ANAME).
@@ -22,12 +27,16 @@ absolute_pathname(LST, OLST) :-
 	atom_codes(PWD, PLST),
 	append([PLST, "/", LST], OLST).
 
+%% filename without directory part
+
 basename(FNAME, BNAME) :-
 	atom(FNAME), atom_codes(FNAME, FLST),
 	basename(FLST, BNAME).
 basename(FNAME, BNAME) :-
 	append(_, BNAME, FNAME),
 	\+memberchk(47, BNAME), !.
+
+%% directory part of filename
 
 dirname(FNAME, DNAME) :-
 	atom(FNAME), atom_codes(FNAME, FLST),
@@ -36,6 +45,8 @@ dirname(FNAME, DNAME) :-
 	append(DNAME, [47|R], FNAME),
 	\+memberchk(47, R), !.
 
+%% file-extension only, fails if filename has no extension
+
 suffix(FNAME, SUF) :-
 	atom(FNAME), atom_codes(FNAME, FLST),
 	suffix(FLST, SUF).
@@ -43,3 +54,13 @@ suffix(FNAME, SUF) :-
 	append(_, SUF, FNAME),
 	\+memberchk(46, SUF), !,
 	FNAME \== SUF.
+
+%% strip file extension
+
+strip_suffix(FNAME, RNAME) :-
+	atom(FNAME), atom_codes(FNAME, FLST),
+	strip_suffix(FLST, RNAME).
+strip_suffix(FNAME, RNAME) :-
+	append(RNAME, [46|R], FNAME),
+	\+memberchk(46, R).
+strip_suffix(FNAME, FNAME).
