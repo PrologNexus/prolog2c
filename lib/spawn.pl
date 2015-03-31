12562aae18eaf740040f1a42fae26e82333d1e0c
felix <felix@call-with-current-continuation.org>
2015-04-01 01:07:34 +0200

    tweaks in lib/spawn - this seems actually to work somewhat

--- lib/spawn.pl
+++ lib/spawn.pl
@@ -4,17 +4,23 @@
 :- verbatim('#include "spawn.c"').
 
 
+%% run subprocess CMD, with list of arguments (not including command)
+%
+% unifies P with process(PID, FDIN, FDOUT)
+
 spawn(CMD, ARGS, P) :-
 	foreign_call(spawn(CMD, ARGS, PID, FDIN, FDOUT)),
 	P = process(PID, FDIN, FDOUT),
 	(recorded(spawned_children, PS, REF), erase(REF); PS = []),
 	!, recordz(spawned_children, [P|PS]).
 
-%% call this periodically
+
+%% call this periodically to reap zombie children
+
 reap_children :-
 	foreign_call(reap_children(PID)),
 	recorded(spawned_children, PS, REF),
-	select(process(P, FDIN, FDOUT), PS, NEWPS), % may fail, if unregistered process terminates
+	select(process(PID, FDIN, FDOUT), PS, NEWPS), % may fail, if unregistered process terminates
 	erase(REF),
 	foreign_call(close_fd(FDIN)),
 	foreign_call(close_fd(FDOUT)),
@@ -22,9 +28,11 @@ reap_children :-
 	reap_children.
 reap_children.
 
-%% forces all data to be written
+
+%% send atom(ic) or string to subprocess, forces all data to be written
+
 send(P, DATA) :-
-	atomic(DATA),
+	atomic(DATA), DATA \= [],
 	!,
 	name(DATA, LST),
 	send(P, LST).
@@ -37,11 +45,16 @@ send_block(FD, DATA) :-
 	skip_items(N, DATA, REST),
 	!, send_block(FD, REST).
 
+%% probably generally useful, find out how this is usually called
 skip_items(0, _, []).
 skip_items(N, [_|R], R2) :-
 	N2 is N - 1,
 	!, skip_items(N2, R, R2).
 
-%% may read fewer than LEN bytes
+
+%% read string from subprocess, may read fewer than LEN bytes
+%
+% will return [] on EOF
+
 receive(process(_, FDIN, _), LEN, BYTES) :-
 	foreign_call(raw_read(FDIN, LEN, BYTES)).
