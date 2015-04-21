4c6440377f6608c7ba09e714c423a0e6b9b9157a
felix <felix@call-with-current-continuation.org>
2015-04-21 14:25:36 +0200

    increased max_arity

--- lib/flags.pl
+++ lib/flags.pl
@@ -5,7 +5,7 @@ current_prolog_flag(version, 5).
 current_prolog_flag(prolog_title, '?-Prolog').
 current_prolog_flag(prolog_copyright, '(c)MMXV Felix L. Winkelmann').
 current_prolog_flag(bounded, true).
-current_prolog_flag(max_arity, 9999). % arbitrary
+current_prolog_flag(max_arity, 65536). % arbitrary
 current_prolog_flag(integer_rounding_function, toward_zero). %XXX correct?
 current_prolog_flag(max_integer, N) :- foreign_call(fixnum_bounds(_, N)).
 current_prolog_flag(min_integer, N) :- foreign_call(fixnum_bounds(N, _)).
