27c3246c803bf9e171cd97c5d0ac51a93351d318
felix <felix@call-with-current-continuation.org>
2015-05-08 15:21:44 +0200

    formatting

--- lib/rdb.pl
+++ lib/rdb.pl
@@ -29,8 +29,11 @@ recorded(KEY, TERM, REF) :-
 	'$record_db_match'(REF1, TERM, REF).
 
 '$record_db_match'(REF, TERM, REF) :-
-	(foreign_call(db_ref(REF, X)) -> X = TERM
-	; garbage_collect, '$record_db_match'(REF, TERM, REF)).
+	( foreign_call(db_ref(REF, X))
+	-> X = TERM
+	; garbage_collect,
+	  '$record_db_match'(REF, TERM, REF)
+	).
 '$record_db_match'(REF1, TERM, REF) :-
 	foreign_call(db_next(REF1, REF2)),
 	!,
