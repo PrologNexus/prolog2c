30b7db1f735e8a34e9831c313fbd1ca9dd291883
felix <felix@call-with-current-continuation.org>
2015-05-08 15:21:38 +0200

    dce: continue eliminating after dropping predicate (do not restart iterating over "defined" rdb entry), also workaround problem with updating rdb-entries while iterating over them

--- dce.pl
+++ dce.pl
@@ -4,21 +4,36 @@
 %% iterate over defined predicates and collect callers, dropping the
 %% predicate if none exists.
 mark_unused_predicates :-
-	mark_unused_predicates(0).
+	recordz(dce_count, 0),
+	mark_unused_predicates_iterate(0),
+	recorded(dce_count, COUNT),
+	message(['% dropping ', COUNT, ' unused predicates']).
 
-mark_unused_predicates(COUNT) :-
+mark_unused_predicates_iterate(COUNT1) :-
+	mark_unused_predicates_iterate,
+	recorded(dce_count, COUNT2),
+	COUNT2 > COUNT1,
+	!,
+	mark_unused_predicates_iterate(COUNT2).
+mark_unused_predicates_iterate(_).
+
+mark_unused_predicates_iterate :-
 	recorded(defined, NA, REF),
 	predicate_callers(NA, []),
 	erase(REF),
 	eliminate_predicate(NA),
-	C2 is COUNT + 1,
-	!,
-	mark_unused_predicates(C2).
-mark_unused_predicates(COUNT) :-
-	message(['% dropped ', COUNT, ' unused predicates']).
+	fail.
+mark_unused_predicates_iterate.	
 
 eliminate_predicate(NA) :-
+	%%write(NA), nl,
 	recorded(calls, calls(NA, _), REF),
 	erase(REF),
 	fail.
-eliminate_predicate(_).
+eliminate_predicate(_) :-
+	recorded(dce_count, N, REF),
+	erase(REF),
+	N2 is N + 1,
+	recordz(dce_count, N2),
+	!.			% avoid backtracking (this is a bug)
+
